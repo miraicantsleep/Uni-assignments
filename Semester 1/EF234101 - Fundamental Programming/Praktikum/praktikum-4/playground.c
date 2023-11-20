78c670ac571a9aec6475fb365c514ad995e79a63
mirai <mnabil190405@gmail.com>
2023-11-21 03:50:19 +0700

    update s4 solped and ac

--- Semester 1/EF234101 - Fundamental Programming/Praktikum/praktikum-4/playground.c
+++ Semester 1/EF234101 - Fundamental Programming/Praktikum/praktikum-4/playground.c
@@ -1,158 +1,102 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-struct mahasiswa
-{
-    char nama[30];
-    char NRP[20];
-    char angkatan[6];
-    char umur[5];
-    char asal[50];
-};
+typedef struct {
+    char nama[101];
+    char nrp[101];
+    char angkatan[101];
+    char umur[101];
+    char asal[101];
+} Mahasiswa;
 
-void printData(struct mahasiswa index, int i)
-{
-    printf("Nama Mhs-%d\t: %s\n", i, index.nama);
-    printf("NRP Mhs-%d\t: %s\n", i, index.NRP);
-    printf("Angkatan Mhs-%d\t: %s\n", i, index.angkatan);
-    printf("Umur Mhs-%d\t: %s\n", i, index.umur);
-    printf("Asal Mhs-%d\t: %s\n\n", i, index.asal);
+void showData(Mahasiswa a, int i) {
+    printf("Nama Mhs-%d\t: %s\n", i, a.nama);
+    printf("NRP Mhs-%d\t: %s\n", i, a.nrp);
+    printf("Angkatan Mhs-%d\t: %s\n", i, a.angkatan);
+    printf("Umur Mhs-%d\t: %s\n", i, a.umur);
+    printf("Asal Mhs-%d\t: %s\n\n", i, a.asal);
 }
 
-void find(struct mahasiswa data[], int size, char check[])
-{
-    bool found = false;
-    for (int i = 0; i < size; i++)
-    {
-        if (strstr(data[i].nama, check) != NULL ||
-            strstr(data[i].NRP, check) != NULL ||
-            strstr(data[i].angkatan, check) != NULL ||
-            strstr(data[i].umur, check) != NULL ||
-            strstr(data[i].asal, check) != NULL)
-        {
-            found = true;
-            printData(data[i], i);
-            break;
-        }
-    }
-    if (!found)
-    {
-        printf("DATA TIDAK DITEMUKAN\n");
-    }
-}
+int main() {
+    int n;
+    scanf("%d\n", &n);
 
-void swapUrut(struct mahasiswa *ptr1, struct mahasiswa *ptr2)
-{
-    struct mahasiswa temp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = temp;
-}
-
-void swapVal(char *ptr1, char *ptr2)
-{
-    char temp[100];
-    strcpy(temp, ptr1);
-    strcpy(ptr1, ptr2);
-    strcpy(ptr2, temp);
-}
-
-int main()
-{
-    int N;
-    scanf("%d", &N);
-    struct mahasiswa structArr[N];
-    for (int i = 0; i < N; i++)
-    {
-        scanf(" %[^\n]", structArr[i].nama);
-        scanf(" %[^\n]", structArr[i].NRP);
-        scanf(" %[^\n]", structArr[i].angkatan);
-        scanf(" %[^\n]", structArr[i].asal);
-        scanf(" %[^\n]", structArr[i].umur);
+    Mahasiswa a[n];
+    for(int i = 0; i < n; i++) {
+        gets(a[i].nama);
+        gets(a[i].nrp);
+        gets(a[i].angkatan);
+        gets(a[i].asal);
+        gets(a[i].umur);
     }
 
-    int command;
-    while (scanf("%d", &command))
-    {
-        if (command == -1)
-        {
-            printf("===== DATA MAHASISWA =====\n");
-            for (int i = 0; i < N; i++)
-            {
+    int apa;
+    while(scanf("%d", &apa) && apa != -1) {
+        if(apa == 1) {
+            char data[101];
+            scanf(" %s", data);
 
-                printData(structArr[i], i);
-            }
-            break;
-        }
-        else if (command == 1)
-        {
+            bool ketemu = false;
             printf("===== HASIL PENCARIAN =====\n");
-            char str[100];
-            scanf(" %[^\n]", str);
-            find(structArr, N, str);
+            for(int i = 0; i < n; i++) {
+                if(strstr(a[i].nama, data) != NULL || strstr(a[i].nrp, data) != NULL || 
+                   strstr(a[i].angkatan, data) != NULL || strstr(a[i].umur, data) != NULL ||
+                   strstr(a[i].asal, data) != NULL) {
+                    ketemu = true;
+                    showData(a[i], i);
+                }
+            }
+            if(!ketemu) {
+                printf("DATA TIDAK DITEMUKAN\n\n");
+            }
         }
-        else if (command == 2)
-        {
-            struct mahasiswa *ptr1;
-            struct mahasiswa *ptr2;
-            int index1;
-            int index2;
-            scanf("%d %d", &index1, &index2);
-            ptr1 = &structArr[index1];
-            ptr2 = &structArr[index2];
-            swapUrut(ptr1, ptr2);
+        else if(apa == 2) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+
+            Mahasiswa temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
         }
-        else if (command == 3)
-        {
-            char swapThis[10];
-            scanf("%s", swapThis);
-            if (strcmp(swapThis, "nama") == 0)
-            {
-                int index1;
-                int index2;
-                scanf("%d %d", &index1, &index2);
-                char *ptr1 = structArr[index1].nama;
-                char *ptr2 = structArr[index2].nama;
-                swapVal(ptr1, ptr2);
+        else {
+            char tukar[101];
+            int x, y;
+            scanf(" %s %d %d", tukar, &x, &y);
+
+            char temp[101];
+            if(strcmp(tukar, "nama") == 0) {
+                strcpy(temp, a[x].nama);
+                strcpy(a[x].nama, a[y].nama);
+                strcpy(a[y].nama, temp);
             }
-            else if (strcmp(swapThis, "nrp") == 0)
-            {
-                int index1;
-                int index2;
-                scanf("%d %d", &index1, &index2);
-                char *ptr1 = structArr[index1].NRP;
-                char *ptr2 = structArr[index2].NRP;
-                swapVal(ptr1, ptr2);
+            else if(strcmp(tukar, "nrp") == 0) {
+                strcpy(temp, a[x].nrp);
+                strcpy(a[x].nrp, a[y].nrp);
+                strcpy(a[y].nrp, temp);
             }
-            else if (strcmp(swapThis, "angkatan") == 0)
-            {
-                int index1;
-                int index2;
-                scanf("%d %d", &index1, &index2);
-                char *ptr1 = structArr[index1].angkatan;
-                char *ptr2 = structArr[index2].angkatan;
-                swapVal(ptr1, ptr2);
+            else if(strcmp(tukar, "angkatan") == 0) {
+                strcpy(temp, a[x].angkatan);
+                strcpy(a[x].angkatan, a[y].angkatan);
+                strcpy(a[y].angkatan, temp);
             }
-            else if (strcmp(swapThis, "umur") == 0)
-            {
-                int index1;
-                int index2;
-                scanf("%d %d", &index1, &index2);
-                char *ptr1 = structArr[index1].umur;
-                char *ptr2 = structArr[index2].umur;
-                swapVal(ptr1, ptr2);
+            else if(strcmp(tukar, "umur") == 0) {
+                strcpy(temp, a[x].umur);
+                strcpy(a[x].umur, a[y].umur);
+                strcpy(a[y].umur, temp);
             }
-            else if (strcmp(swapThis, "asal") == 0)
-            {
-                int index1;
-                int index2;
-                scanf("%d %d", &index1, &index2);
-                char *ptr1 = structArr[index1].asal;
-                char *ptr2 = structArr[index2].asal;
-                swapVal(ptr1, ptr2);
+            else {
+                strcpy(temp, a[x].asal);
+                strcpy(a[x].asal, a[y].asal);
+                strcpy(a[y].asal, temp);
             }
         }
     }
+
+    printf("===== DATA MAHASISWA =====\n");
+    for(int i = 0; i < n; i++) {
+        showData(a[i], i);
+    }
+
+    return 0;
 }
